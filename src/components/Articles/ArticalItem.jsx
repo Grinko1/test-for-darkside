0db36a869aa39be310e7152c33857f7a958d4002
Grinko1 <86093873+Grinko1@users.noreply.github.com>
2022-05-24 13:05:23 +0300

    format code

--- src/components/Articles/ArticalItem.jsx
+++ src/components/Articles/ArticalItem.jsx
@@ -1,19 +1,17 @@
 import './ArticleItem.css';
 
-const ArticalItem = ({title, text, time}) => {
+const ArticalItem = ({ title, text, time }) => {
+  const prev = text.split('').slice(0, 250).join('') + '...';
+  const date = new Date(time);
+  const formattedDate = Intl.DateTimeFormat().format(date);
 
-
-  const prev = text.split('').slice(0, 250).join('') + '...'
-      const date = new Date(time);
-      const formattedDate = Intl.DateTimeFormat().format(date);
-
-    return (
-        <div className='article'>
-            <h3 className='article__title'>{title}</h3>
-            <p className='preview'> {prev}</p>
-            <p className='article__date'>{formattedDate}</p>
-        </div>
-    );
+  return (
+    <div className="article">
+      <h3 className="article__title">{title}</h3>
+      <p className="preview"> {prev}</p>
+      <p className="article__date">{formattedDate}</p>
+    </div>
+  );
 };
 
 export default ArticalItem;
